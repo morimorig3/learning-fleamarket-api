ef4363a0f6b62dcb3b1de05e7ec70bc7afff978c
morimorig3 <mori.nto@outlook.jp>
2025-02-01 09:53:28 +0900

    feat: ItemRepository作成

---
+++ repositories/item_repository.go
@@ -0,0 +1,23 @@
+package repositories
+
+import "learning-freemarket-api/models"
+
+type IItemRepository interface {
+	FindAll() (*[]models.Item, error)
+}
+
+// ↑↑↑↑↑のFindAllを満たすための実装が↓↓↓↓↓↓
+type ItemMemoryRepository struct {
+	items []models.Item
+}
+
+// ファクトリ関数の戻り値の方をインターフェースにすることで
+// 具体的な実装がインターフェースの型を満たしていないときに
+// エラーが発生するので実装もれを防ぐことができる
+func NewItemMemoryRepository(items []models.Item) IItemRepository {
+	return &ItemMemoryRepository{items: items}
+}
+
+func (r *ItemMemoryRepository) FindAll() (*[]models.Item, error) {
+	return &r.items, nil
+}
